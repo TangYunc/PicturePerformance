80ad7dee115b0415f6041f7b8a1a3c7856abd78d
Mr_tang <18242312660@163.com>
2018-06-08 15:26:38 +0800

    绘制圆形图像以及边界，解决图像性能问题

--- PicturePerformance/ViewController.swift
+++ PicturePerformance/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    ///如何设置圆角，不要用圆角半径
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +23,7 @@ class ViewController: UIViewController {
         //设置图像，PNG图片是支持透明的
         let imageIcon = #imageLiteral(resourceName: "avatar_default_big")
         
-        iv.image = avatarImage(image: imageIcon, size: rect.size)
+        iv.image = avatarImage(image: imageIcon, size: rect.size, backGroundColor: view.backgroundColor)
     }
 
     /// 将给定的图像进行拉伸，并且返回‘新的’图像
@@ -31,7 +32,7 @@ class ViewController: UIViewController {
     ///   - image: 将要绘制的图片
     ///   - size: 绘图的尺寸
     /// - Returns: UIImage
-    func avatarImage(image: UIImage, size: CGSize) -> UIImage? {
+    func avatarImage(image: UIImage, size: CGSize, backGroundColor: UIColor?) -> UIImage? {
         let rect = CGRect(origin: CGPoint(), size: size)
        //1.图像的上下文，内存中开辟地址，与屏幕无关
         /**
@@ -40,8 +41,21 @@ class ViewController: UIViewController {
          peaque:不透明 false / true
          scale：屏幕分辨率，默认生成的图像默认使用1.0的分辨率，图像质量不好。所以可以指定 0，即会选择当前屏幕的分辨率
          */UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+        //0.背景填充
+        backGroundColor?.setFill()
+        UIRectFill(rect)
+        //1.实例化一个圆形的的路径
+        let path = UIBezierPath(ovalIn: rect)
+        //2.进行路径裁切， -- 后续的绘图都会出现在路径内部，外面的全部干掉
+        path.addClip()
+       
         //绘制drawInRect就是在指定区域内拉伸屏幕
         image.draw(in: rect)
+        //绘制内切的圆形
+        //设置边线
+        UIColor.darkGray.setStroke()
+        path.lineWidth = 2
+        path.stroke()
         //取得结果
         let result =  UIGraphicsGetImageFromCurrentImageContext()
         //关闭上下文
